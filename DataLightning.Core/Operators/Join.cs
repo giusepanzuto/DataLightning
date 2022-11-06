8eb44909909d2a5ada19c9de57750bb4f9d16fdc
Giuseppe Panzuto <giusepanzuto@gmail.com>
2022-11-06 15:06:00 +0100

    renamed Join2 to Join

--- DataLightning.Core/Operators/Join.cs
+++ DataLightning.Core/Operators/Join.cs
@@ -4,8 +4,11 @@ using System.Linq;
 
 namespace DataLightning.Core.Operators
 {
-    public class Join<TLeftInput, TRightInput> : CalcUnitBase<object, (IList<TLeftInput> Left, IList<TRightInput> Right)>
+    public class Join<TLeftInput, TRightInput> : ISubscribable<(IList<TLeftInput> Left, IList<TRightInput> Right)>
     {
+        private readonly SubscriptionManager<(IList<TLeftInput> Left, IList<TRightInput> Right)> _subscriptionManager = new();
+        private readonly InputManager _inputManager = new();
+
         private readonly (object Key, Func<TLeftInput, object> KeyGetter, Dictionary<object, Dictionary<object, TLeftInput>> Data) _left;
         private readonly (object Key, Func<TRightInput, object> KeyGetter, Dictionary<object, Dictionary<object, TRightInput>> Data) _right;
         private readonly Func<TLeftInput, object> _leftEntityKeyGetter;
@@ -14,52 +17,48 @@ namespace DataLightning.Core.Operators
         public Join(
             ISubscribable<TLeftInput> leftInput, ISubscribable<TRightInput> rightInput,
             Func<TLeftInput, object> leftKeyGetter, Func<TRightInput, object> rightKeyGetter,
-            Func<TLeftInput, object> leftEntityKeyGetter, Func<TRightInput, object> rightEntityKeyGetter) :
-            base(new ISubscribable<object>[] {
-                new SubscriberAdapter<TLeftInput, object>(leftInput),
-                new SubscriberAdapter<TRightInput, object>(rightInput) })
+            Func<TLeftInput, object> leftEntityKeyGetter, Func<TRightInput, object> rightEntityKeyGetter) 
         {
+            _inputManager.Add("left", leftInput, (_, value) => OnLeftChanged(value));
+            _inputManager.Add("right", rightInput, (_, value) => OnRightChanged(value));
+
             _left = (Key: leftInput, KeyGetter: leftKeyGetter, Data: new Dictionary<object, Dictionary<object, TLeftInput>>());
             _right = (Key: rightInput, KeyGetter: rightKeyGetter, Data: new Dictionary<object, Dictionary<object, TRightInput>>());
             _leftEntityKeyGetter = leftEntityKeyGetter;
             _rightEntityKeyGetter = rightEntityKeyGetter;
         }
 
-        protected override (IList<TLeftInput> Left, IList<TRightInput> Right) Execute(IDictionary<object, object> args, object changedInput)
+        private void OnRightChanged(TRightInput rightInput)
         {
-            if (args[changedInput] == null)
-                return (new List<TLeftInput>(), new List<TRightInput>());
+            var key = _right.KeyGetter(rightInput);
+            if (!_right.Data.ContainsKey(key))
+                _right.Data[key] = new Dictionary<object, TRightInput>();
+            _right.Data[key][_rightEntityKeyGetter(rightInput)] = rightInput;
 
-            object key;
-
-            if (changedInput == _left.Key || (changedInput is SubscriberAdapter<TLeftInput, object> leftAdapter && leftAdapter.Adaptee == _left.Key))
-            {
-                TLeftInput leftInput = (TLeftInput)args[changedInput];
-                key = _left.KeyGetter(leftInput);
-                if (!_left.Data.ContainsKey(key))
-                    _left.Data[key] = new Dictionary<object, TLeftInput>();
-                _left.Data[key][_leftEntityKeyGetter(leftInput)] = leftInput;
-
-                if (_right.Data.ContainsKey(key))
-                    return (_left.Data[key].Values.ToList(), _right.Data[key].Values.ToList());
-
-                return (_left.Data[key].Values.ToList(), new List<TRightInput>());
-            }
-            else if (changedInput == _right.Key || (changedInput is SubscriberAdapter<TRightInput, object> rightAdapter && rightAdapter.Adaptee == _right.Key))
-            {
-                TRightInput rightInput = (TRightInput)args[changedInput];
-                key = _right.KeyGetter(rightInput);
-                if (!_right.Data.ContainsKey(key))
-                    _right.Data[key] = new Dictionary<object, TRightInput>();
-                _right.Data[key][_rightEntityKeyGetter(rightInput)] = rightInput;
-
-                if (_left.Data.ContainsKey(key))
-                    return (_left.Data[key].Values.ToList(), _right.Data[key].Values.ToList());
-
-                return (new List<TLeftInput>(), _right.Data[key].Values.ToList());
-            }
+            if (_left.Data.ContainsKey(key))
+                _subscriptionManager.NotifySubscribers(
+                    (_left.Data[key].Values.ToList(), _right.Data[key].Values.ToList()));
             else
-                throw new ArgumentException();
+                _subscriptionManager.NotifySubscribers(
+                    (new List<TLeftInput>(), _right.Data[key].Values.ToList()));
         }
+
+        private void OnLeftChanged(TLeftInput leftInput)
+        {
+            var key = _left.KeyGetter(leftInput);
+            if (!_left.Data.ContainsKey(key))
+                _left.Data[key] = new Dictionary<object, TLeftInput>();
+            _left.Data[key][_leftEntityKeyGetter(leftInput)] = leftInput;
+
+            if (_right.Data.ContainsKey(key))
+                _subscriptionManager.NotifySubscribers(
+                    (_left.Data[key].Values.ToList(), _right.Data[key].Values.ToList()));
+            else
+                _subscriptionManager.NotifySubscribers(
+                    (_left.Data[key].Values.ToList(), new List<TRightInput>()));
+        }
+
+        public ISubscription Subscribe(ISubscriber<(IList<TLeftInput> Left, IList<TRightInput> Right)> subscriber) => 
+            _subscriptionManager.Subscribe(subscriber);
     }
 }
